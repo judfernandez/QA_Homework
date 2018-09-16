334817c5c98bcdf96d9c42a663853081f66680a8
Juan David Fernández Moreno <judfernandezmo@unal.edu.co>
2018-09-16 11:54:26 -0500

    olo

---
+++ springexample/src/test/java/springexample/TestNgMavenExampleTest.java
@@ -0,0 +1,10 @@
+package springexample;
+import org.testng.annotations.Test;
+
+public class TestNgMavenExampleTest {
+
+	@Test
+	public void exampleOfTestNgMaven() {
+		System.out.println("This is TestNG-Maven Example");
+	}
+}
